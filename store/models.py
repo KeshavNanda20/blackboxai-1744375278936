462794fa3d0612d1db88c0c8ec4c5c6f4cd9aff6
KeshavNanda20 <115359498+KeshavNanda20@users.noreply.github.com>
2025-04-28 18:26:04 +0530

    Update models.py

--- store/models.py
+++ store/models.py
@@ -61,6 +61,8 @@ class Order(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     total = models.DecimalField(max_digits=10, decimal_places=2)
+    delivery_address = models.TextField(blank=True, null=True)
+    rating = models.PositiveIntegerField(null=True, blank=True)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
